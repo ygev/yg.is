57b0f843578e7b65d5e184ea1cd29447ed76ceac
yana <ygev@outlook.com>
2019-01-20 12:42:14 -0500

    Remove / from everything.

--- dist/js/filter.js
+++ dist/js/filter.js
@@ -1,8 +1,3 @@
 var containerEl = document.querySelector('.filter-container');
 
 var mixer = mixitup(containerEl);
-
-// shadow scroll
-// document.querySelector('div').onscroll = function () {
-//     this.classList[this.scrollRight > 10 ? 'add' : 'remove']('overlay-gradient');
-// };
