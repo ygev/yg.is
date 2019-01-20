7aaa58715f9a57eb3755718aae9c1da53428dd94
yana <ygev@outlook.com>
2019-01-20 12:17:28 -0500

    First commit.

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = {
+    dir: {
+        input: "src",
+        output: "dist"
+    },
+    htmlTemplateEngine: "njk",
+    templateFormats: ["html", "njk", "css", "js", "jpg", "png", "svg"]
+};
---
+++ dist/js/filter.js
@@ -0,0 +1,8 @@
+var containerEl = document.querySelector('.filter-container');
+
+var mixer = mixitup(containerEl);
+
+// shadow scroll
+// document.querySelector('div').onscroll = function () {
+//     this.classList[this.scrollRight > 10 ? 'add' : 'remove']('overlay-gradient');
+// };
