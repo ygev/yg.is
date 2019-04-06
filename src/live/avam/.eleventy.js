d215e1f9fa0025cd310e98c36f273bf125025f90
yana <ygev@outlook.com>
2019-04-05 23:10:13 -0400

    Move stuff away to their own repos, haha.

--- src/live/avam/.eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    dir: {
-        input: "src",
-        output: "dist"
-    },
-    htmlTemplateEngine: "njk",
-    templateFormats: ["html", "njk", "css", "js", "jpg", "png", "svg"]
-};
