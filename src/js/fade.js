c8787afb2613fdf9b4b30fc44a4fc81457e3f796
yana <ygev@outlook.com>
2019-04-07 01:31:58 -0400

    Add progress bar and hover style for link.

--- src/js/fade.js
+++ src/js/fade.js
@@ -3,6 +3,20 @@ $(function () {
     $('body').removeClass('fade-out');
 });
 
+
+//progress scroll 
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () {
+    myFunction()
+};
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+
 // fade as scroll
 
 // $(function () { // $(document).ready shorthand
