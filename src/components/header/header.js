73eff1f11f44c29e44173d9e06d8c4140ed10416
Yana <yanacommit@wizwar.net>
2020-02-03 12:33:10 -0800

    Add normalize.css + innerHero.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "../../images/logo.svg" 
 import "./header.css"
-import "../../css/reset.css"
+import "../../css/normalize.css"
+import "../../css/global.css"
 
 export default props => (
 <div className="wrapper-header">
-   <img className="img-logo" src={logo} alt=""></img>
-   <a className="navlink__active" href="#" alt="">Projects</a>
-   <a className="navlink" href="#" alt="">About</a>
-   </div>
+   <nav>
+      <img className="img-logo" src={logo} alt=""></img>
+      <a className="navlink__active" href="#" alt="">Projects</a>
+      <a className="navlink" href="#" alt="">About</a>
+   </nav>
+</div>
 ); 
--- src/components/innerHero/innerHero.js
+++ src/components/innerHero/innerHero.js
@@ -1,9 +1,24 @@
 import React from "react";
-import "./innerHero.css"
+import "./innerHero.css";
+import "../../css/normalize.css"
+import "../../css/global.css"
+import hero from "../../images/inner/hero.png"; 
+import gitlogo from "../../images/inner/gitlogo.svg"
 
 export default props => (
     <>
-        <h3 className="brow">ux design - 2019 - web application</h3>
-        <h2 className="lead">Raising Awareness About Cybersecurity by Creating a Targeted Phishing Tool</h2>
+        <section className="hero">
+        <figure className="hero__img"> 
+            <img src={hero} alt="load butthole"/>
+        </figure>
+        <div className="hero__txt"> 
+            <h3 className="brow">ux design - 2019 - web application</h3>
+            <h2 className="lead">Raising Awareness About Cybersecurity by Creating a Targeted Phishing Tool</h2>
+            <div className="btn__git--wrapper">
+                <button className="btn__git">View Repository</button>
+                <span className="btn__git--iconbg"><img className="btn__git--icon" src={gitlogo}/></span>
+            </div>
+        </div>
+        </section>
     </>
-);
+);  
