2a31f96d47faed2b7d2b507a3f2279c975bd6fe0
Yana <yanacommit@wizwar.net>
2020-02-06 11:10:18 -0800

    Move tracker scrollmagic to tracker.js

--- src/components/tracker/tracker.js
+++ src/components/tracker/tracker.js
@@ -3,14 +3,18 @@ import "./tracker.css";
 import "../../css/normalize.css";
 import "../../css/global.css";
 import up from "../../images/inner/up.svg"; 
+import { Controller, Scene } from "react-scrollmagic"
 
 export default props => (
     <>
-
+        <Controller>
+            <Scene triggerHook="onLeave" pin>
                 <section className="tracker">
                     <h4 className="tracker__head">{props.trackerHead} </h4>
                     <img src={up} className="tracker__icon" alt=""/>
                 </section>
+            </Scene>
+        </Controller>
 
     </>
 
