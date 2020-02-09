b83df69e98237920a4d19460b39f58dbd1e4e964
Yana <yanacommit@wizwar.net>
2020-02-09 14:54:42 -0800

    Gridify abstract + paragraph.

--- src/components/paragraph/paragraph.js
+++ src/components/paragraph/paragraph.js
@@ -5,15 +5,17 @@ import "../../css/global.css";
 
 export default props => (
     <>
-    <section className="paragraph">
-        <article className="paragraph__section">
-            <h4 className="paragraph__title">
-            {props.paragraphTitle}
-            </h4>
-            <p className="paragraph__body">
-            {props.paragraphBody} 
-            </p>
-        </article>
-    </section>
+    <div className="gridContainer">
+        <section className="paragraph">
+            <article className="paragraph__section">
+                <h4 className="paragraph__title">
+                {props.paragraphTitle}
+                </h4>
+                <p className="paragraph__body">
+                {props.paragraphBody} 
+                </p>
+            </article>
+        </section>
+    </div>
     </>
 );  
