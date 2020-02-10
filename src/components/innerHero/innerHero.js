6d9745d0518965dc9f8101ae0af70b540521b640
Yana <yanacommit@wizwar.net>
2020-02-09 20:52:23 -0800

    Install parallax tilt library for hero image.

--- src/components/innerHero/innerHero.js
+++ src/components/innerHero/innerHero.js
@@ -3,12 +3,19 @@ import "./innerHero.css";
 import "../../css/normalize.css";
 import "../../css/global.css";
 import hero from "../../images/inner/hero.png"; 
+import Tilt from 'react-parallax-tilt';
 
 export default props => (
     <>
         <section className="hero">
             <figure className="hero__img"> 
+                <Tilt
+                perspective={500}  
+                gyroscope={true}
+                scale={1.02}
+                trackOnWindow={true}>
                 <img src={hero} alt="load butthole"/>
+                </Tilt>
             </figure>
         </section>
     </>
