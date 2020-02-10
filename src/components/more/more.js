4da4ce2b7bb058ddcd050e5a969420c45c7f430e
Yana <yanacommit@wizwar.net>
2020-02-09 18:18:36 -0800

    Make the base desktop + mobile + tablet responsive

--- src/components/more/more.js
+++ src/components/more/more.js
@@ -9,11 +9,29 @@ export default props => (
         <section className="more">
             <h4 className="more__head">Check Out More
             </h4>
-            <figure className="">
-            <img className="img-glamor" src={glamor} alt=""/>
-            <figcaption className="more__title">{props.heroTitle}</figcaption>
-            <h4 className="more__head">{props.heroCategory} - {props.heroYear} - {props.heroDeliverable}</h4>
-            </figure>
+            <div className="more__group">
+                <figure className="more__item">
+                    <img className="img-glamor" src={glamor} alt=""/>
+                    <figcaption className="more__title">{props.heroTitle}
+                    </figcaption>
+                    <h4 className="more__head">{props.heroCategory} - {props.heroYear} - {props.heroDeliverable}
+                    </h4>
+                </figure>
+                <figure className="more__item">
+                    <img className="img-glamor" src={glamor} alt=""/>
+                    <figcaption className="more__title">{props.heroTitle}
+                    </figcaption>
+                    <h4 className="more__head">{props.heroCategory} - {props.heroYear} - {props.heroDeliverable}
+                    </h4>
+                </figure>
+                <figure className="more__item">
+                    <img className="img-glamor" src={glamor} alt=""/>
+                    <figcaption className="more__title">{props.heroTitle}
+                    </figcaption>
+                    <h4 className="more__head">{props.heroCategory} - {props.heroYear} - {props.heroDeliverable}
+                    </h4>
+                </figure>
+            </div>
         </section>
     </>
 );  
