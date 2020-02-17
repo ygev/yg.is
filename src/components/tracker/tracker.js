275691c5cc458f948c4c5df58e63268d7b1118d7
Yana <yanacommit@wizwar.net>
2020-02-16 18:58:03 -0800

    Add variable phase function.

--- src/components/tracker/tracker.js
+++ src/components/tracker/tracker.js
@@ -17,42 +17,16 @@ export default props => (
                             <h4 className="tracker__head">{props.trackerHead} </h4>
                         </div>
                         <div className="phases">
-                            <Controller>
-                                <Scene  duration={() => { if (document.getElementById('p1') != null) return document.getElementById('p1').clientHeight } }
-                                        classToggle="phases__active" triggerElement="#p1" indicators={true}>
-                                    <h3 className="phases__txt">PHASE 1: BOOBIE</h3>
-                                </Scene>
-                            </Controller>
-                            <Controller>
-                                <Scene  duration={() => { if (document.getElementById('p2') != null) return document.getElementById('p2').clientHeight } }
-                                        classToggle="phases__active" triggerElement="#p2" indicators={true}>
-                                    <h3 className="phases__txt">PHASE 2: BOOBIE</h3>
-                                </Scene>
-                            </Controller>                            
-                            <Controller>
-                                <Scene  duration={() => { if (document.getElementById('p3') != null) return document.getElementById('p3').clientHeight } }
-                                        classToggle="phases__active" triggerElement="#p3" indicators={true}>
-                                    <h3 className="phases__txt">PHASE 3: BOOBIE</h3>
-                                </Scene>
-                            </Controller>                            
-                            <Controller>
-                                <Scene  duration={() => { if (document.getElementById('p4') != null) return document.getElementById('p4').clientHeight } }
-                                        classToggle="phases__active" triggerElement="#p4" indicators={true}>
-                                    <h3 className="phases__txt">PHASE 4: BOOBIE</h3>
-                                </Scene>
-                            </Controller>                            
-                            <Controller>
-                                <Scene  duration={() => { if (document.getElementById('p5') != null) return document.getElementById('p5').clientHeight } }
-                                        classToggle="phases__active" triggerElement="#p5" indicators={true}>
-                                    <h3 className="phases__txt">PHASE 5: BOOBIE</h3>
-                                </Scene>
-                            </Controller>
-                            <Controller>
-                                <Scene  duration={() => { if (document.getElementById('p6') != null) return document.getElementById('p6').clientHeight } }
-                                        classToggle="phases__active" triggerElement="#p6" indicators={true}>
-                                    <h3 className="phases__txt">PHASE 6: BOOBIE</h3>
-                                </Scene>
-                            </Controller>
+                            {
+                                Array.from(Array(props.phaseList.length).keys()).map((i) => {
+                                    return <Controller>
+                                        <Scene   duration={() => { if (document.getElementById('p' + (i+1)) != null) return document.getElementById('p' + (i+1)).clientHeight } }
+                                                    classToggle="phases__active" triggerElement={'#p' + (i+1)} indicators={true}>
+                                        <h3 className="phases__txt">PHASE {i+1}: {props.phaseList[i]}</h3>
+                                        </Scene>
+                                    </Controller>;
+                                })
+                            }
                         </div>
                     </section>
                 </Scene>
@@ -60,5 +34,4 @@ export default props => (
         </Fade>
     </>
 
-    
 );  
--- src/pages/phishing/index.js
+++ src/pages/phishing/index.js
@@ -33,9 +33,10 @@ class Index extends React.Component {
                   abstractTools="Sqlite.js · E.js · Express.js" 
                   abstractTime="14 days · September 2019" 
                   abstractBody="MICA Phisher is a proof-of-concept phishing site which spoofs Maryland Institute College of Art’s login page. It exposes the relative ease of creating tools to obtain sensitive information from unsuspecting users."/>
-        <Tracker trackerHead="Raising Awareness About Cybersecurity by Creating a Targeted Phishing Tool"/>
+        <Tracker  phaseList={['UNDERSTAND', 'DEFINE', 'IDEATE', 'ITERATE', 'VALIDATE']}
+                  trackerHead="Raising Awareness About Cybersecurity by Creating a Targeted Phishing Tool"/>
         <div id="p1">
-        <Phase phaseTitle="Phase 1"/>
+        <Phase phaseTitle="Understanding the Problem"/>
         <Questions questionTitle="Research Questions"
                    questionItem1="How have attitudes towards OPSEC changed nation-wide, given recent data breaches and hacks?" 
                    questionItem2="To what extent do students at Maryland Institute College of Art use password managers?" 
@@ -55,28 +56,23 @@ class Index extends React.Component {
                    questionItem2="Students at the Maryland Institute College of Art largely do not use password managers, nor do they see the importance of two factor authentication that has been established in 2019. " 
                    questionItem3="Students mindlessly enter their passwords without double checking the URL first. Frequent bugs in MICA’s system force students to enter their credentials twice, which is an explotable feature." />
          </div>
-         <div id="p2">
-        <Phase phaseTitle="Phase 2"/>
+        <div id="p2">
+        <Phase phaseTitle="Defining the Problem"/>
         <Paragraph paragraphTitle="Lorem Dipshit" 
                    paragraphBody="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur felis ipsum, tincidunt sit amet metus ac, aliquet accumsan ligula. Ut vestibulum odio commodo ligula ultrices, ac vulputate ante finibus. Proin vitae felis porttitor, condimentum turpis sed, commodo tortor. Maecenas ullamcorper neque sem, sit amet tincidunt nisl pellentesque vel. Integer pharetra dolor vel nunc tempus, a mattis augue vehicula. Cras facilisis enim et enim rutrum rhoncus. Aenean ornare lacus ut nulla tristique interdum. Aenean congue eros elit."/>
         </div>
         <div id="p3">
-        <Phase phaseTitle="Phase 3"/>
+        <Phase phaseTitle="Ideation"/>
         <Paragraph paragraphTitle="Lorem Dipshit" 
                    paragraphBody="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur felis ipsum, tincidunt sit amet metus ac, aliquet accumsan ligula. Ut vestibulum odio commodo ligula ultrices, ac vulputate ante finibus. Proin vitae felis porttitor, condimentum turpis sed, commodo tortor. Maecenas ullamcorper neque sem, sit amet tincidunt nisl pellentesque vel. Integer pharetra dolor vel nunc tempus, a mattis augue vehicula. Cras facilisis enim et enim rutrum rhoncus. Aenean ornare lacus ut nulla tristique interdum. Aenean congue eros elit."/>
         </div>
         <div id="p4">
-        <Phase phaseTitle="Phase 4"/>
+        <Phase phaseTitle="Prototyping Ideas"/>
         <Paragraph paragraphTitle="Lorem Dipshit" 
                    paragraphBody="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur felis ipsum, tincidunt sit amet metus ac, aliquet accumsan ligula. Ut vestibulum odio commodo ligula ultrices, ac vulputate ante finibus. Proin vitae felis porttitor, condimentum turpis sed, commodo tortor. Maecenas ullamcorper neque sem, sit amet tincidunt nisl pellentesque vel. Integer pharetra dolor vel nunc tempus, a mattis augue vehicula. Cras facilisis enim et enim rutrum rhoncus. Aenean ornare lacus ut nulla tristique interdum. Aenean congue eros elit."/>
         </div>
         <div id="p5">
-        <Phase phaseTitle="Phase 5"/>
-        <Paragraph paragraphTitle="Lorem Dipshit" 
-                   paragraphBody="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur felis ipsum, tincidunt sit amet metus ac, aliquet accumsan ligula. Ut vestibulum odio commodo ligula ultrices, ac vulputate ante finibus. Proin vitae felis porttitor, condimentum turpis sed, commodo tortor. Maecenas ullamcorper neque sem, sit amet tincidunt nisl pellentesque vel. Integer pharetra dolor vel nunc tempus, a mattis augue vehicula. Cras facilisis enim et enim rutrum rhoncus. Aenean ornare lacus ut nulla tristique interdum. Aenean congue eros elit."/>
-        </div>
-        <div id="p6">
-        <Phase phaseTitle="Phase 6"/>
+        <Phase phaseTitle="Validating the Results"/>
         <Paragraph paragraphTitle="Lorem Dipshit" 
                    paragraphBody="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur felis ipsum, tincidunt sit amet metus ac, aliquet accumsan ligula. Ut vestibulum odio commodo ligula ultrices, ac vulputate ante finibus. Proin vitae felis porttitor, condimentum turpis sed, commodo tortor. Maecenas ullamcorper neque sem, sit amet tincidunt nisl pellentesque vel. Integer pharetra dolor vel nunc tempus, a mattis augue vehicula. Cras facilisis enim et enim rutrum rhoncus. Aenean ornare lacus ut nulla tristique interdum. Aenean congue eros elit."/>
         </div>
