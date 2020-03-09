a43c64ae9469c4b1dbff12e8dfcb2ad6f80e2790
Yana <yanacommit@wizwar.net>
2020-03-08 23:24:21 -0700

    Add image matrix and adjust lead for 4K.

--- src/components/abstract/abstract.js
+++ src/components/abstract/abstract.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./abstract.css";
 import "../../css/normalize.css"
 import "../../css/global.css"
-import glamour from "../../images/inner/glamourPH.png" 
+import glamour from "../../images/inner/phisher-login.webm" 
+import glamour1 from "../../images/inner/phisher-fullscreen.png" 
+import glamour2 from "../../images/inner/phisher-fish.webm" 
 import caret_left from "../../images/inner/caret_left.svg" 
 import Fade from 'react-reveal/Fade';
 
@@ -10,7 +12,24 @@ export default props => (
     <>
         <Fade bottom delay={500} duration={300}>
             <section className="abstract">
-                <img className="img-glamour" src={glamour} alt=""/>
+                <section className="abstract__gallery">
+                    <div className="abstract__img--wrapper" data-width="1">
+                        <img className="abstract__img"  src={glamour1}/>
+                    </div>
+                    <div className="abstract__img--wrapper" data-width="2">
+                        <video className="abstract__img" preLoad="yes" autoplay="autoplay" loop muted playsinline>
+                            <source src={glamour} type="video/webm"/>
+                        </video>
+                        <video className="abstract__img" preLoad="yes" autoplay="autoplay" loop muted playsinline>
+                            <source src={glamour2} type="video/webm"/>
+                        </video>
+                    </div>
+                    <div className="abstract__img--wrapper" data-width="3">
+                        <img className="abstract__img" src="https://via.placeholder.com/960x960"/>
+                        <img className="abstract__img" src="https://via.placeholder.com/960x960"/>
+                        <img className="abstract__img" src="https://via.placeholder.com/960x960"/>
+                    </div>
+                </section>
                 <section className="abstract__txt--wrapper">
                     <aside className="abstract__txt--left">
                     <section className="abstract__details">
--- src/components/innerHero/innerHero.js
+++ src/components/innerHero/innerHero.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./innerHero.css";
 import "../../css/normalize.css";
 import "../../css/global.css";
-import Tilt from 'react-parallax-tilt';
 import Fade from 'react-reveal/Fade';
 
 export default props => (
@@ -10,13 +9,7 @@ export default props => (
         <Fade left duration={500}>
             <section className="hero">
                 <figure className="hero__img"> 
-                    <Tilt
-                    perspective={1000}  
-                    gyroscope={true}
-                    scale={1.02}
-                    trackOnWindow={true}>
                     <img src={props.img} alt=""/>
-                    </Tilt>
                 </figure>
             </section>
         </Fade>
