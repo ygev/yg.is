34484146bfcdbb43b56db8751a9f523859abb4ad
Yana <yanacommit@wizwar.net>
2020-03-09 17:12:10 -0700

    Change all <a> to <Link>

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,16 +5,17 @@ import "./header.css"
 import "../../css/normalize.css"
 import "../../css/global.css"
 import Fade from 'react-reveal/Fade';
+import {Link} from "gatsby"
 
 export default props => (
 <Fade right duration={500}>
    <div className="header__wrapper">
       <nav>
-         <Logo className="spinny-logo"/>
+         <Link to="/"><Logo className="spinny-logo"/></Link>
          <img className="img-logo" src={logo} alt=""></img>
          <section className="header__txt">
-         <a className="navlink__active" href="#" alt="">Projects</a>
-         <a className="navlink" href="#" alt="">About</a>
+         <Link className="navlink__active" to="">Projects</Link>
+         <Link className="navlink" to="">About</Link>
          </section>
       </nav>
    </div>
