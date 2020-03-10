50ae0c956d4736f7fa474c62408d823193badec6
Yana <yanacommit@wizwar.net>
2020-03-09 18:22:05 -0700

    Make contact.js into links with hover.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -9,8 +9,10 @@ export default props => (
             <h4 className="contact__head">Feel free to say hi!
             </h4>
             <div className="btn__contact--wrapper">
-                <button className="btn__contact">LinkedIn</button>
-                <button className="btn__contact">Resume</button>
+                <a href="mailto:ygis@mit.edu"><button className="btn__contact">ygis@mit.edu</button></a>
+                <a href="https://linkedin.com/ygevorgyan"><button className="btn__contact">LinkedIn</button></a>
+                <a href="https://github.com/ygev"><button className="btn__contact">GitHub</button></a>
+                <a href="https://are.na/yana"><button className="btn__contact">Are.Na</button></a>
             </div>
         </section>
     </>
