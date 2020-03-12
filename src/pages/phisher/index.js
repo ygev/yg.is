3c1a734e3124af30ca153d614c782f497f17b55e
Yana <yanacommit@wizwar.net>
2020-03-12 01:02:02 -0700

    Phisher: Add highlights and links.

--- src/pages/phisher/index.js
+++ src/pages/phisher/index.js
@@ -72,13 +72,13 @@ class Index extends React.Component {
                               "To what extent do people at Maryland Institute College of Art use password managers?", 
                               "To what extend do people mindlessly enter personal data into credible interfaces?"]} />
         <Paragraph paragraphTitle="Surveying National Opsec Engagement" 
-                  paragraphBody="According to a study by the Pew Research Center, as of 2016, most Americans kept track of their online passwords by either memorizing them or writing them down. However, with subsequent data breaches, such as Capital One and Equifax, the popularity of password managers rose up. In 2019 a Threatpost poll found that 84% of respondents use password managers, with only 10 percent of people saying that they don't due to perceived security risks."/>
+                  paragraphBody="According to a [[study>https://www.pewresearch.org/internet/2017/01/26/americans-and-cybersecurity/]] by the Pew Research Center, as of 2016, most Americans kept track of their online passwords by either memorizing them or writing them down. However, with subsequent data breaches, such as Capital One and Equifax, the popularity of password managers rose up. In 2019 a Threatpost [[poll>https://threatpost.com/poll-password-managers-risky/142114/]] found that <<84% of respondents use password managers,>> with only 10 percent of people saying that they don't due to perceived security risks."/>
         <ImgRight img={Fig1} imgNum="01" imgCaption="Pew Research Center, 2019"/>
         <Paragraph paragraphTitle="Surveying Students' Opsec Engagement" 
-                  paragraphBody="Even Maryland Institute College of Art's website implemented two-factor authentication for their students in 2019 with variable feedback from the students. I have informally surveyed 15 students and plotted the most insightful responses on a scale from positive to negative."/>
+                  paragraphBody="Even Maryland Institute College of Art's website implemented two-factor authentication for their students in 2019 with variable feedback from the students. I have informally <<surveyed 15 students>> and plotted the most insightful responses on a scale from positive to negative."/>
         <ImgFull img={Fig2} imgNum="02" imgCaption="Plotted testimonies from MICA students about the two-factor authentication system established in Fall 2019."/>
         <Paragraph paragraphTitle="Considering Mindless UI Patterns"   
-                  paragraphBody="The main cause of phishing is the lack of user training. Especially in college, with weak Wi-Fi signal, students are used to mindlessly entering their password more than once without looking at the URL twice. This mindless data entry is an aspect of human behavior that can and is exploited."/>
+                  paragraphBody="<<The main cause of phishing is the lack of user training.>> Especially in college, with weak Wi-Fi signal, students are used to mindlessly entering their password more than once without looking at the URL twice. This mindless data entry is an aspect of human behavior that can and is exploited."/>
         </div>
         <div id="p2">
         <Phase phaseTitle="Defining the Problem"/>
@@ -90,33 +90,33 @@ class Index extends React.Component {
                               "Students mindlessly enter their passwords without double checking the URL first. Frequent bugs in MICA’s system force students to enter their credentials twice, which is an explotable feature."]} />
         <ImgQuote img={Fig3}/>
         <Paragraph paragraphTitle="Lack of Awareness" 
-                  paragraphBody="Based on these insights, I’ve concluded there is not enough awareness about how easy it is to steal one’s data on campus compared to the national average presentd by the Pew Research Center. Phisher websites can be generated programmatically and it is far too easy to store passwords in a database with malicious intentions, even by an art student like me. Students largely didnt care much for the two-factor authentication employed by the school after a number of phishing attacks. There was clearly not enough awareness around the campus, leading me to pose the following design challenge: "/>
+                  paragraphBody="Based on these insights, I’ve concluded there is not enough awareness about how easy it is to steal one’s data on campus compared to the national average presented by the Pew Research Center. Phisher websites can be generated programmatically and it is far too easy to store passwords in a database with malicious intentions, even by an art student like me. <<Students largely didnt care much for the two-factor authentication>> employed by the school after a number of phishing attacks. There was clearly not enough awareness around the campus, leading me to pose the following design challenge: "/>
         <HowMightWe howMightWe="How might we raise on-campus awareness about phishing through an experiential medium while maintaining safety?"/>
         </div>
         <div id="p3">
         <Phase phaseTitle="Brainstorming Awareness Campaigns"/>  
         <Paragraph paragraphTitle="" 
-                  paragraphBody="I decided to think about how people unlearn poor user behaviors. After a mind-mapping exercise, I came up with three main ideas between which my ideal solution would lie."/>
+                  paragraphBody="I decided to think about how people unlearn poor user behaviors. After a <<mind-mapping exercise>>, I came up with three main ideas between which my ideal solution would lie."/>
         <ImgFull img={Fig5} imgNum="03" imgCaption="Mind-mapping exercise that led to the project's three main foci."/>
         <ImgLeft img={Fig4} imgNum="04" imgCaption="The solution lies at the intersection of Visual Routines, Experiential Learning and Gesamkunstwerk"/>
         <Paragraph paragraphTitle="Visual Routines" 
-                  paragraphBody="As discovered, people are relying on previously learned habits to fall for phishing tricks. In order to be effective, my solution needs to reveal the presence of these routines and allow people to notice them in the future."/>
+                  paragraphBody="As discovered, people are relying on <<previously learned habits>> to fall for phishing tricks. In order to be effective, my solution needs to reveal the presence of these routines and allow people to notice them in the future."/>
         <Paragraph paragraphTitle="Experiential Learning" 
-                  paragraphBody="People should learn from experience. In order to be effective, my solution must be a piece that can be interacted with: something that either is or resembles a phishing website."/>
-        <Paragraph paragraphTitle="Gesamkunstwerk" 
-                  paragraphBody="The solution is forming up to be a type of a phishing site. Since my primary audience are my student peers, the site should target students from the Maryland Institute College of Art. I wanted to create a back-end interface that shows an infographic of which websites have been caught."/>
+                  paragraphBody="<<People grasp new technologies through experience.>> In order to be effective, my solution must be a piece that can be interacted with: something that either is or resembles a phishing website."/>
+        <Paragraph paragraphTitle="Gesamtkunstwerk" 
+                  paragraphBody="The experiential quality of the solution will be successful because of the <<totality of the experience>>, or gesamtkunstwerk. The students will log in to the same login page they use every day, but instead of being taken to the home page, they will be taken to a page that creates a <<meta-commentary on their naiveté.>>"/>
         <Paragraph paragraphTitle="" 
-                  paragraphBody="With these parameters in mind, I set out to create what I think is an engaging experiment: a phishing site that targets students from the Maryland Institute College of Art and displays the number of passwords caught since launch once the user logs in. The site will consist of a <<login page>> and a <<page that displays the stolen passwords.>> The user will be redirected to the page after they enter their password and press enter, as per this user flow diagram:"/>
+                  paragraphBody="With these parameters in mind, I set out to create what I think is an engaging experiment: a phishing site that targets students from the Maryland Institute College of Art and displays the number of passwords caught since launch once the user logs in. The site will consist of a <<login page>> and the previously mentioned <<meta page.>> The user will be redirected to the page after they enter their password and press enter, as per this user flow diagram:"/>
          <ImgRight img={FigFlow} imgNum="05" imgCaption="Simple user flow for displaying the credentials caught by MICA Phisher"/>
         </div>
         <div id="p4">
         <Phase phaseTitle="Prototyping Phishing Site Layout"/>
         <ImgLeft img={Fig6} imgNum="06" padding="true" imgCaption="MICA’s Communication Toolkit"/>
         <Paragraph paragraphTitle="Brandjacking" 
-                  paragraphBody="I decided to use MICA’s branding designed by Pentagram as the visual language, identity and style guide for the alternate landing page. I created a logo in the style of MICA’s sub-brands."/>
+                  paragraphBody="I decided to use MICA’s branding designed by Pentagram as the visual language, identity and style guide for the alternate landing page. Like many style guidelines, it was available publicly and marketed as the [[MICA Communication Toolkit>>https://www.mica.edu/offices-divisions/division-of-strategic-communications/toolkit/branding/]]. I created a logo in the style of MICA’s sub-brands to follow the <<common phishing practice of brandjacking.>>"/>
         <ImgRight img={Fig7} imgNum="07" padding="true" imgCaption="MICA’s Sub-Brands, now including Phisher."/>
         <Paragraph paragraphTitle=""
-                          paragraphBody="Although there is a plethora of colors to choose from, I picked the top three colors seen on MICA’s front page, since this is something that has already been prioritized before me by the original creators of the site."/>
+                          paragraphBody="Although there is a plethora of colors to choose from, I picked the top three colors seen on [[MICA’s front page>https://www.mica.edu]], since this is something that has already been prioritized before me by the original creators of the site."/>
         <ImgRight img={Fig8} imgNum="08" padding="true" imgCaption="MICA’s Home Page displaying the most commonly used colors by the brand."/>
         <Paragraph paragraphTitle="Wireframing the Displays" 
                   paragraphBody="I started sketching out the pass-word display screen. The following information needed to be displayed:"/>
