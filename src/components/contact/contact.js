f610298e6bc56db0031e4f23c829a6921692124f
Yana <yanacommit@wizwar.net>
2020-03-13 19:49:36 -0700

    Fix link to Linkedin in contact.js

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -10,7 +10,7 @@ export default props => (
             </h4>
             <div className="btn__contact--wrapper">
                 <a href="mailto:ygis@mit.edu"><button className="btn__contact">ygis@mit.edu</button></a>
-                <a href="https://linkedin.com/ygevorgyan"><button className="btn__contact">LinkedIn</button></a>
+                <a href="https://linkedin.com/in/ygevorgyan"><button className="btn__contact">LinkedIn</button></a>
                 <a href="https://github.com/ygev"><button className="btn__contact">GitHub</button></a>
                 <a href="https://are.na/yana"><button className="btn__contact">Are.Na</button></a>
             </div>
