d36125bde5b5d9e8fffc9517af6427360d45b770
kian <kian@horsefucker.org>
2020-05-02 17:38:53 -0700

    Remove path prefix, which fixes github pages deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    pathPrefix: "/ygis",
+    pathPrefix: "/",
     siteMetadata: {
       title: "Yana Gevorgyan",
       titleTemplate: "%s · YG.IS",
       description:
         "Yana Gevorgyan is a Seattle-based product designer, developer and theorist.",
-      url: "https://www.yg.is", 
-      image: "/images/logo.png", 
+      url: "https://www.yg.is",
+      image: "/images/logo.png",
     },
     plugins: [
       `gatsby-plugin-transition-link`
