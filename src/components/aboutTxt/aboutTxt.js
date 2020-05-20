04401d2ce71167254bd32fa3de258134b137ed1a
Yana <yanacommit@wizwar.net>
2020-05-19 17:52:28 -0700

    Add day-of-week based greeting to About page.

--- src/components/aboutTxt/aboutTxt.js
+++ src/components/aboutTxt/aboutTxt.js
@@ -7,13 +7,28 @@ import gitlogo from "../../images/inner/gitlogo.svg";
 import devpostlogo from "../../images/inner/devpostlogo.svg";
 import Fade from 'react-reveal/Fade';
 
+function setDayMessage() {
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Happy Sunday!";
+    weekday[1] = "Hope you're having a good Monday!";
+    weekday[2] = "Hope you're having a good Tuesday!";
+    weekday[3] = "Happy Hump Day!";
+    weekday[4] = "It's almost Friday!";
+    weekday[5] = "Thank God it's Friday!";
+    weekday[6] = "Happy Saturday!";
+
+    var n = weekday[d.getDay()];
+    return n;
+}
+
 export default props => (
     <>
         <Fade duration={500}>
             <div className="aboutTxt"> 
-                <h3 className=" aboutTxt__brow">Thank God It's Friday!</h3>
-                <h2 className=" aboutTxt__lead">Y. G. is Yana Gevorgyan 👋</h2>
-                <h2 className=" aboutTxt__head">Based in Seattle, I am a multi-disciplinary product designer bridging people with tech.  When not maintaining my open-source projects, you'll find me arranging black metal compositions on the piano, writing about design theory on my Medium and buying dumb domain names. Seeking opportunities.</h2>
+                <h3 className=" aboutTxt__brow">{setDayMessage()}</h3>
+                <h2 className=" aboutTxt__lead">I'm Yana Gevorgyan 👋</h2>
+                <h2 className=" aboutTxt__head">Based in Seattle, I am a multi-disciplinary product designer bridging people with tech.  When not maintaining my open-source projects, you'll find me arranging black metal compositions on the piano buying dumb domain names. Seeking opportunities.</h2>
             </div>
         </Fade>
     </>
