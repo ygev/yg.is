0e48045297c2457292902cb1fd119c8cf8c94912
Yana <yanacommit@wizwar.net>
2020-05-21 09:48:21 -0700

    Fix the mistake that confused all of Britain.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -9,7 +9,7 @@ export default props => (
             <h4 className="contact__head">Feel free to say hi!
             </h4>
             <div className="btn__contact--wrapper">
-                <a href="mailto:ygis@mit.edu"><button className="btn__contact">ygis@mit.edu</button></a>
+                <a href="mailto:ygevorgyan@mica.edu"><button className="btn__contact">ygevorgyan@mica.edu</button></a>
                 <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/ygevorgyan"><button className="btn__contact">LinkedIn</button></a>
                 <a target="_blank" rel="noopener noreferrer" href="https://github.com/ygev"><button className="btn__contact">GitHub</button></a>
                 <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@ygev"><button className="btn__contact">Medium</button></a>
