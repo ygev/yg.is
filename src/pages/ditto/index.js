63f160fe6fb711e2b48b696309069fa987a7bc25
Yana <yanacommit@wizwar.net>
2020-05-28 00:50:39 -0700

    Complete final write-up for Ditto. No proofreading

--- src/pages/ditto/index.js
+++ src/pages/ditto/index.js
@@ -13,6 +13,18 @@ import Glam6 from "./images/Glam6.png";
 import Fig1 from "./images/Fig1.png";
 import Fig2 from "./images/Fig2.svg";
 import Fig3 from "./images/Fig3.svg";
+import Fig4 from "./images/Fig6.jpg";
+import Fig5 from "./images/Fig13.svg";
+import Fig6 from "./images/Fig9.png";
+import Fig7 from "./images/Fig8.png";
+import Fig8 from "./images/Fig10.png";
+import Fig9 from "./images/Fig7.png";
+import Fig10 from "./images/Fig11.gif";
+import Fig11 from "./images/Fig14.png";
+import Fig12 from "./images/Fig15.png";
+import Fig13 from "./images/Fig5.png";
+import Fig14 from "./images/Fig16.png";
+import Fig15 from "./images/Fig12.png";
 import "../../css/global.css"
 import Header from "../../components/header/header"
 import InnerHero from "../../components/innerHero/innerHero"
@@ -99,7 +111,7 @@ class Index extends React.Component {
         <Tracker  phaseList={['UNDERSTAND', 'DEFINE', 'DECIDE', 'PROTOTYPE', 'VALIDATE']}
                   trackerHead={Constants.ditto.heroTitle}/>
         <div id="p1">
-          <Phase phaseTitle="Understanding a Unique Use Case"/>
+          <Phase phaseTitle="Understanding a Unique Use Case for Sketch"/>
           <Paragraph paragraphTitle="Designing Backwards" 
                     paragraphBody="Normally, product prototyping tools like Sketch and Figma are used to create designs that will end up on the web, rather than copying what is on the web into the prototype. However, this use case exists, and I was tasked with creating a tool that performs this workflow more efficiently, using my skillsets in UX design and UI engineering.
                     "/>
@@ -114,21 +126,26 @@ class Index extends React.Component {
         <div id="p2">
           <Phase phaseTitle="Defining the Pain Points of the Team's Workflow"/>
           <Paragraph paragraphTitle="Copy-Pasting Galore" 
-                    paragraphBody="The impetus for this project is an opportunity to accelerate the team's workflow. Currently, UI designers at Yext take up to a minute per font-style. 
+                    paragraphBody="The impetus for this project is an opportunity to accelerate the team's workflow. Currently, UI designers at Yext take up to a minute per font-style. Their CSS-to-Sketch workflow consists of relying heavily on their own memory and Chrome Dev Tools to manually find every property of a font and copy it to Sketch, from font-family to letter-spacing. This gets very tedious, especially when you are working on several clients a day and copying up to a dozen fonts per client. 
                     "/>
           <ImgFull img={Fig2} imgNum="02" imgCaption="Current workflow of UI Designers at Yext. Highlighted in red are areas for improvement."/>
           <Paragraph paragraphTitle="Desired Workflow Acceleration" 
-                    paragraphBody="The impetus for this project is an opportunity to accelerate the team's workflow. Currently, UI designers at Yext take up to a minute per font-style. 
+                    paragraphBody="I compiled a workflow graphic for me to analyse and highlighted areas that were most stressful to the team. Afterwards, I thought about aspects of it can be automated. Copying and pasting between Chrome and Sketch are the most tedious sections for the designers and are the easiest to implement. Unfortunately, I cannot do anything about switching windows between the two programs, other than limit the number of times one will have to do it.
                     "/>
           <ImgFull img={Fig3} imgNum="03" imgCaption="Desired workflow adjustments thanks to the design accelerator."/>
-          <Paragraph paragraphBody="The impetus for this project is an opportunity to accelerate the team's workflow. Currently, UI designers at Yext take up to a minute per font-style. "/>
+          <Paragraph paragraphBody="After generating an updated flowchart of the workflow adjustments, my problem was defined, and I conceived a How Might We statement to help direct my future brainstorming and prototyping vision."/>
           <HowMightWe howMightWe="How might we optimize the unique CSS-to-Sketch workflow of UI designers at Yext?"/>
         </div>
         <div id="p3">
           <Phase phaseTitle="Deciding on the Project's Scope"/>
-          <Paragraph paragraphTitle="Identify the Medium of the Design Accelerator" 
-                    paragraphBody="The impetus for this project is an opportunity to accelerate the team's workflow. Currently, UI designers at Yext take up to a minute per font-style. 
+          <ImgLeft img={Fig4} imgNum="04" imgCaption="Razer Naga Trinity, what I use for hotkeys in Sketch." />
+          <Paragraph paragraphTitle="A Note on Design Accelerators" 
+                    paragraphBody="I never truly appreciated plugins and hotkeys before starting to work in an environment as fast paced as Yext. Design accelerators can cut down on a lot of time. For instance, hotkeys can reduce a task from clicking through a series of nested menus to one click. In fact, some designers on my team, including me, use gaming mice with programmable hotkeys, such as the Razer Naga Trinity. Therefore, I kept in mind the team's love of hotkeys when designing this tool. 
                     "/>
+          <Paragraph paragraphTitle="What medium is best for this design accelerator?" 
+                    paragraphBody="Since the workflow deals with Chrome and Sketch, it seems that it will either have to be a Chrome Extension or a Sketch Plugin. After doing some research on the functionalities of both, a software engineering intern and I decided that it will have to be a combination of the two. We outlined what actions the extension will do in Chrome and what the plugin will do in Sketch:
+                    "/>
+          <ImgRight img={Fig5} imgNum="" imgCaption=""/>
           <Paragraph paragraphTitle="What properties define a font's identity?" 
                     paragraphBody="In order to accurately copy the essence of a font, we needed to determine what makes a font style in CSS. These properties must also have equivalents in Sketch. For instance, the font-family property of CSS should correspond to the Typefaces field in Sketch. The final list of properties that are most commonly specified in CSS and make a font instantly recognizable as a distinct style are as follows:
                     "/>
@@ -136,28 +153,68 @@ class Index extends React.Component {
                 listItems={["font-size",
                 "color",
                 "line-height",
-                "font-weight"
+                "font-weight",
+                "text-decoration",
+                "font-family",
+                "font-style",
+                "letter-spacing",
+                "text-transform",
+                "font-variant",
+                "text-shadow"
                 ]} /> 
         </div>
         <div id="p4">
-          <Phase phaseTitle="Prototyping the Extension"/>
-          <Paragraph paragraphTitle="Interface of the Modal" 
-                    paragraphBody="The impetus for this project is an opportunity to accelerate the team's workflow. Currently, UI designers at Yext take up to a minute per font-style. 
+          <Phase phaseTitle="Conceiving the Initial Release"/>
+          <ImgLeft img={[Fig6, Fig7, Fig8]} imgNum="" imgCaption=""/>
+          <Paragraph paragraphTitle="Downselection of Font-Properties for Display" 
+                    paragraphBody="The primary interface of the Chrome extension is the modal with the Clipboard History that appears once the extension is activated. I identified font-family, font-weight and font-size as the three most important features of a font that will be displayed on the pop-up Clipboard History. The font properties that are currently in the clipboard will be highlighted in pink following the Google Material Design Guidelines and accompanied with a Material Icon of a copy symbol.
                     "/>
+          
+          <ImgRight img={Fig9} imgNum="" imgCaption=""/>
+          <Paragraph paragraphTitle="Developing the Chrome Extension and Sketch Plugin" 
+                    paragraphBody="After the interface was complete, I had a hand-off with Blake Quigley. Within a week, he developed the initial code base for both the Chrome Extension and the Sketch Plugin, incorporating dropdown animations we have discussed during the handoff. The plugin entered the QA section of the development process and we asked the UI Design team to use it for a week and note any bugs on GitHub.
+                    "/>
+          <ImgRight img={Fig10} />
+          <ImgLeft img={Fig12} padding="yes" imgNum="05" imgCaption="Published on the Chrome Web Store under Klepto."/>
           <Paragraph paragraphTitle="Publishing to Chrome Store" 
-                    paragraphBody="The impetus for this project is an opportunity to accelerate the team's workflow. Currently, UI designers at Yext take up to a minute per font-style. 
+                    paragraphBody="After bug fixes and QA testing, the project was published to the Chrome Store under the moniker Klepto with an icon of a pink flamboyant raccoon. It was highly successful with the UI Design team and had 38 users within a month of publishing. 
                     "/>
         </div>
         <div id="p5">
           <Phase phaseTitle="Validation and Post-Internship Maintenance"/>
-          <Paragraph paragraphTitle="Interface of the Modal" 
-                    paragraphBody="The impetus for this project is an opportunity to accelerate the team's workflow. Currently, UI designers at Yext take up to a minute per font-style. 
+          <Paragraph paragraphTitle="A Paragon of Iterative Design" 
+                    paragraphBody="After graduating from the internship, I gained more skills in both web development, plugin creation, as well as visual design. I noticed that no one was maintaining this extension and took initiative to redesign it to make it more usable. I forked the repository and fixed a number of bugs, as well as removed code that was not doing anything. After speaking with other designers, as well as observing the plugin myself, I identified the following visual design issues: 
                     "/>
-          <Paragraph paragraphTitle="Interface of the Modal" 
-                    paragraphBody="The impetus for this project is an opportunity to accelerate the team's workflow. Currently, UI designers at Yext take up to a minute per font-style. 
+          <Issue img={Fig11} issueTitle="Poor Color Contrast" issueItems={["Cannot see white font-families.", "Light colored fonts invisible"]}/>
+          <Issue img={Fig9} issueTitle="Poor Visibility of Modal" issueItems={["Most websites are light-mode.", "Cannot find modal easily."]}/>
+          <Issue img={Fig6} issueTitle="No Directions for Hotkey" issueItems={["Don't know how to copy font.", "How to activate the extension?"]}/>
+          <Paragraph 
+                    paragraphBody="Additionally, the project lacked good usage directions and was never officially published to Sketch's plugin repository, which limited its accessibliity outside of the UI Design team's reach.
                     "/>
-          <Paragraph paragraphTitle="Branding and Logotype" 
-                    paragraphBody="The impetus for this project is an opportunity to accelerate the team's workflow. Currently, UI designers at Yext take up to a minute per font-style. 
+          <Paragraph paragraphTitle="Usability Overhaul" 
+                    paragraphBody="I took time to address each problem and came up with a solution for the Chrome Extension modal that solves every issue noted by the users and myself during the heuristic evaluation.
+                    "/>
+           <ImgLeft img={Hero} imgNum="" imgCaption=""/>
+           <List listTitle="Features that Solved the Usability Issues" listType='ul'
+                listItems={["<<Dark-Mode Interface>> The dark mode interface proved to be more visible for users on most websites and was better suited for the plugin. Perhaps in the future, I can use the new CSS option to detect the user's screen-mode and toggle dark-mode whenever appropriate.",
+                "<<Hotkey Directions in the Title>> I put the hotkey combination directly in the title of the modal, so that it was more clear what the next step was after activating the extension.",
+                "<<Static Color for all Fonts>> Letting the color of all fonts display as white in the clipboard history allows all fonts to be perfectly visible. Selecting a black font will not obscure the font-family name due to poor color contrast. Moreover, this change makes for better accessibility, since that color combination is sure to pass WCAG AAA."
+                ]} /> 
+
+           
+          <ImgLeft img={[Fig13, Fig14]} imgNum={["06","07"]} imgCaption={["Brainstorming the optimal file structure of the repo.", "Website design of Ditto."]}/>
+          <Paragraph paragraphTitle="Creating a Monorepo" 
+                    paragraphBody="Additionally, I cleaned up the file structure on GitHub and made it clear for those who are trying to read the code, or even download the latest release without relying on Chrome's Web Store and Sketch's Plugin Repository.
+                    "/>
+          <Paragraph paragraphTitle="Creating a Brand Identity and Website" 
+                    paragraphBody="I dubbed my updated plugin Ditto, a name that has a less negative connotation than Klepto, and a name that the UI Design team really enjoyed during our brainstorming sessions. I teamed up with my visual designer friend, Amanda Yeh, and developed a promotional website for the plugin that displays a new brand identity based on the dark-mode of the new Chrome extension.
+                    "/>
+          <Paragraph paragraphTitle="Future Developments" 
+                    paragraphBody="I hope to some day merge Ditto with another Chrome Extension I made: Banditto. Banditto fetches webfonts from websites and downloads them to your computer with a single click. As this can technically be used as a piracy tool, I feel uneasy going through with this, but the idea is still amusing to think about.
+                    "/>
+          <ImgRight img={Fig15} imgNum="08" imgCaption="Banditto in action, nabbing a font from Hoefler via a context-menu. Very unethical."/>
+          <Paragraph paragraphTitle="Subsequent Response from Yext" 
+                    paragraphBody="In May 2020, I have officially reached out to Tyler Anderson, the Senior UI Designer at Yext to see if I can take over the maintenance of the plugin, since no one has touched it since the end of my internship. He, as well as the Creative Director of the UI Team, Erin Pfiffner, were completely on board and commended my efforts to keep the plugin active. I am waiting on the final okay from Yext's legal team to push the updates to the Chrome Web Store!
                     "/>
         </div>
         <More/>
