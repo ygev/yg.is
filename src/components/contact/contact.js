c34616bfcba8bf16cc81f29ae2134716537e4a7a
Yana <yanacommit@wizwar.net>
2020-10-25 16:19:15 -0700

    Add link to are.na instead of Medium. Trve.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -12,7 +12,7 @@ export default props => (
                 <a href="mailto:ygevorgyan@mica.edu"><button className="btn__contact">ygevorgyan@mica.edu</button></a>
                 <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/ygevorgyan"><button className="btn__contact">LinkedIn</button></a>
                 <a target="_blank" rel="noopener noreferrer" href="https://github.com/ygev"><button className="btn__contact">GitHub</button></a>
-                <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@ygev"><button className="btn__contact">Medium</button></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://are.na/yana"><button className="btn__contact">Are.Na</button></a>
             </div>
             <p className="contact__txt">I'll show you cooler, NDA-compliant projects privately, if we're a match.</p>
         </section>
