8f4dd6734dc712b3b72beab3a22751f16b830e61
Yana <yanacommit@wizwar.net>
2020-12-07 17:45:58 -0800

    Move Are.na up top and Linkedin to the bottom.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -10,9 +10,9 @@ export default props => (
             </h4>
             <div className="btn__contact--wrapper">
                 <a href="mailto:ygevorgyan@mica.edu"><button className="btn__contact">ygevorgyan@mica.edu</button></a>
-                <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/ygevorgyan"><button className="btn__contact">LinkedIn</button></a>
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/ygev"><button className="btn__contact">GitHub</button></a>
                 <a target="_blank" rel="noopener noreferrer" href="https://are.na/yana"><button className="btn__contact">Are.Na</button></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/ygev"><button className="btn__contact">GitHub</button></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/ygevorgyan"><button className="btn__contact">LinkedIn</button></a>
             </div>
         </section>
     </>
