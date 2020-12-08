fa10bc8bab06c23ba8770cccf79c6fbdc544fffc
Yana <yanacommit@wizwar.net>
2020-12-07 17:42:34 -0800

    Add Amanda's gif to the footer.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./footer.css";
 import "../../css/normalize.css";
 import "../../css/global.css";
+import cat from "../../images/vibing-cat.gif"
 
 export default props => (
     <>
         <section className="footer">
             <h4 className="footer__txt">
-            2020 © Yana Gevorgyan 🖤 <a rel="noopener noreferrer" target="_blank" href="https://www.github.com/ygev/yg.is" className="footer__txt--link">Open-Source Portfolio</a>
+            <a rel="noopener noreferrer" target="_blank" href="https://www.github.com/ygev/yg.is" className="footer__txt--link">Open-Source Portfolio</a> · My Heart Belongs to <a rel="noopener noreferrer" target="_blank" href="https://www.amandayeh.com" ><img className="footer__img" src={cat} alt="Gif of vibing cat."/></a>
             </h4>
         </section>
     </>
