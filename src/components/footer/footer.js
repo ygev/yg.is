53191256d41c05562030b88392ae81d0b05de52a
Yana <yanacommit@wizwar.net>
2020-12-07 17:54:46 -0800

    Remove www. from Amanda's link.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,7 +8,7 @@ export default props => (
     <>
         <section className="footer">
             <h4 className="footer__txt">
-            <a rel="noopener noreferrer" target="_blank" href="https://www.github.com/ygev/yg.is" className="footer__txt--link">Open-Source Portfolio</a> · My Heart Belongs to <a rel="noopener noreferrer" target="_blank" href="https://www.amandayeh.com" ><img className="footer__img" src={cat} alt="Gif of vibing cat."/></a>
+            <a rel="noopener noreferrer" target="_blank" href="https://www.github.com/ygev/yg.is" className="footer__txt--link">Open-Source Portfolio</a> · My Heart Belongs to <a rel="noopener noreferrer" target="_blank" href="https://amandayeh.com" ><img className="footer__img" src={cat} alt="Gif of vibing cat."/></a>
             </h4>
         </section>
     </>
