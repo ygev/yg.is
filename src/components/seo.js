64f8619c3580893bd0255d0ee95f2645591ca1fc
yana <ygevorgyan@mica.edu>
2021-06-06 19:39:14 -0700

    Fuck the algorithm.

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,6 +5,7 @@ import { Helmet } from "react-helmet"
 export default props =>  (
   <Helmet titleTemplate="%s · YG.IS">
     <meta charSet="utf-8" />
+    <meta name="robots" content="noindex">
     <meta name="description" content={props.description}/>
     <meta name="author" content="Yana G."/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
