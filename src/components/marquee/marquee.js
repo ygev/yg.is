e8ae77130493c2699d65e6866823dce89a740abe
ygev <1816215+ygev@users.noreply.github.com>
2022-08-30 18:32:19 -0700

    Marquee text.

--- src/components/marquee/marquee.js
+++ src/components/marquee/marquee.js
@@ -5,7 +5,7 @@ import "../../css/global.css"
 import Marquee from "react-fast-marquee";
 
 export default function FooComponent() {
-    var newPortfolio =  <div className="marquee__prefix">· 2020 PORTFOLIO ·<a className="marquee__spacing" target="_blank" rel="noopener noreferrer" href="https://ygev.github.io/Trojan.JS.YouAreAnIdiot/"> VISIT NEW SITE</a></div>
+    var newPortfolio =  <div className="marquee__prefix">· THIS IS A 2020 PORTFOLIO ·<a className="marquee__spacing" target="_blank" rel="noopener noreferrer" href="https://ygev.github.io/Trojan.JS.YouAreAnIdiot/"> VISIT MY NEW SITE</a></div>
     const linkSpam = Array(1000).fill(newPortfolio);
     
   return (
